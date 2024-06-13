03a982191b43f158460639c83da3ce69a6535b73
coronelee <a.tirki90@gmail.com>
2024-06-13 07:25:13 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,9 @@ const telegram = new TelegramWebAppContainer();
 
 telegram.WebApp.ready();
 const first_name = telegram.WebApp.initDataUnsafe?.user?.first_name;
+
+const tg = window.Telegram.WebApp;
+tg.expand();
 </script>
 
 <template>
