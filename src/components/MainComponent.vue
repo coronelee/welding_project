51dbd5864cc03041055eafd47ab3c40f1c4814b4
coronelee <a.tirki90@gmail.com>
2024-06-13 06:05:51 +0300

    add

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,15 +2,15 @@
 <input type="file" id="file" class="hidden" @change="onFileChange">
 <input type="file" id="camera" accept="image/*" capture="camera" class="hidden" @change="onFileChange">
      <div class="w-full h-full flex justify-center items-center flex-col p-2 gap-2 animate-[showMain_0.5s_ease_1]">
-        <div class="text-3xl font-mono w-[calc(100%-58px)] h-full bg-center bg-no-repeat bg-contain" :style="{backgroundImage: `url(${imageSrc})`}"></div>
-        <div v-if="!showCamera" class="w-full h-[50px] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
-            <button class="bg-[url('/images/download.svg')]"  @click="showCamera = !showCamera"></button>
+        <div  class="text-3xl font-mono w-[calc(100%-58px)] h-full bg-center bg-no-repeat bg-contain" :style="{backgroundImage: `url(${imageSrc})`}"></div>
+        <div v-if="!showCamera" id="nav" class="transition-all duration-300 w-full h-[50px] animate-[showNav_0.3s_ease_1] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
+            <button class="bg-[url('/images/download.svg')]"  @click="showNav()"></button>
             <button class="bg-[url('/images/help.svg')]"></button>
             <button class="bg-[url('/images/top.svg')]"></button>
             <button class="bg-[url('/images/profile.svg')]"></button>
             
         </div>
-        <div v-if="showCamera" class="w-full w-full h-[50px] flex gap-4 justify-center items-center [&>button]:w-full [&>button]:h-full rounded-lg [&>button]:bg-[#D9D9D9]">
+        <div v-if="showCamera" id="navFile" class="w-full w-full transition-all duration-300 animate-[showNav_0.3s_ease_1] h-[50px] flex gap-4 justify-center items-center [&>button]:w-full [&>button]:h-full rounded-lg [&>button]:bg-[#D9D9D9]">
             <button @click="downloadImage('camera')">Сделать фото</button> <button @click="downloadImage('file')">Выбрать из галереи</button>
         </div>
      </div>
@@ -22,6 +22,13 @@ const imageSrc = ref('/images/noImage.jpg');
 const showCamera = ref(false);
 const loadImage = () => {
     let input = document.createElement('input');
+}
+
+const showNav = () => {
+    document.getElementById('nav').style.transform = 'translateY(150%)';
+    setTimeout(() => {
+        showCamera.value = !showCamera.value; 
+    }, 300)
 }
 const downloadImage = (id) => {
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ export default {
         showMain: {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
-        }
+        },
+        showNav: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
       }
     },
   },
