4a1cb1040252d82c177685da853971792259a822
coronelee <a.tirki90@gmail.com>
2024-06-13 05:11:55 +0300

    ad

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,5 @@ const first_name = telegram.WebApp.initDataUnsafe?.user?.first_name;
 </script>
 
 <template>
-  <div class="w-screen h-screen pb-16"><component :is="currentView" :first_name="first_name"/></div>
+  <div class="w-screen h-screen pb-12"><component :is="currentView" :first_name="first_name"/></div>
 </template>
