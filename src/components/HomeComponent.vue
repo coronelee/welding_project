e2b39e15b2908449bd8394dd1630fe599a07de12
coronelee <a.tirki90@gmail.com>
2024-06-13 05:45:44 +0300

    add

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="home" class="bg-[url('/images/splash.svg')] transition-all duration-1000 bg-cover bg-no-repeat w-full h-full flex justify-center items-center flex-col gap-8">
+    <div id="home" class="transition-all duration-1000 bg-cover bg-no-repeat w-full h-full flex justify-center items-center flex-col gap-8">
       <span class="text-5xl font-mono text-white">Привет {{ first_name }}!</span>
       <button class="text-3xl bg-white px-6 py-3 rounded-lg" @click="swapPage">Start</button>
     </div>
@@ -18,3 +18,12 @@ const swapPage = () => {
   }, 1000);
 }
 </script>
+
+
+<style scoped>
+#home {
+    background-size: 100% 100%;
+    background-position: 0px 0px,0px 0px,3px 0px;
+    background-image: radial-gradient(49% 81% at 94% 7%, #4c0029 0%, #073AFF00 100%),radial-gradient(113% 91% at 13% 54%, #49779FFF 1%, #FF000000 99%),radial-gradient(142% 91% at 1% 58%, #000000FF 99%, #FF000000 99%);
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,7 +3,7 @@
 <input type="file" id="camera" accept="image/*" capture="camera" class="hidden" @change="onFileChange">
      <div class="w-full h-full flex justify-center items-center flex-col p-2 gap-2 animate-[showMain_0.5s_ease_1]">
         <div class="text-3xl font-mono w-[calc(100%-58px)] h-full bg-center bg-no-repeat bg-contain" :style="{backgroundImage: `url(${imageSrc})`}"></div>
-        <div class="w-full absolute bottom-0 h-[50px] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
+        <div class="w-full absolute bottom-2 h-[50px] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
             <button class="bg-[url('/images/download.svg')]"  @click="downloadImage('file')"></button>
             <button class="bg-[url('/images/help.svg')]"></button>
             <button class="bg-[url('/images/top.svg')]"></button>
