15733d914706da89d0f431cbe948ee638688f15c
coronelee <a.tirki90@gmail.com>
2024-06-13 07:00:57 +0300

    add

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,42 +3,25 @@
 <input type="file" id="camera" accept="image/*" capture="camera" class="hidden" @change="onFileChange">
      <div class="w-full h-full flex justify-center items-center flex-col p-2 gap-2 animate-[showMain_0.5s_ease_1]">
         <div  class="text-3xl font-mono w-[calc(100%-58px)] h-full bg-center bg-no-repeat bg-contain" :style="{backgroundImage: `url(${imageSrc})`}"></div>
-        <div v-if="!showNavFile" id="nav" class="transition-all duration-300 w-full h-[50px] animate-[showNav_0.3s_ease_1] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
-            <button class="bg-[url('/images/download.svg')]"  @click="showNav()"></button>
-            <button class="bg-[url('/images/help.svg')]"></button>
-            <button class="bg-[url('/images/top.svg')]"></button>
-            <button class="bg-[url('/images/profile.svg')]"></button>
-        </div>
-        <div v-if="showNavFile" id="navFile" class="w-full w-full transition-all duration-300 animate-[showNav_0.3s_ease_1] h-[50px] flex gap-4 justify-center items-center [&>button]:w-full [&>button]:h-full rounded-lg [&>button]:bg-[#D9D9D9]">
-            <button @click="downloadImage('camera')">Сделать фото</button> <button @click="downloadImage('file')">Выбрать из галереи</button>
-        </div>
+        <NavBar :downloadImage="downloadImage" :onFileChange="onFileChange" :closeNavFile="closeNavFile"/>
      </div>
 </template>
 
 <script setup>
+import NavBar from "./NavBar.vue";
 import { ref } from "vue";
 const imageSrc = ref('/images/noImage.jpg');
-const showNavFile = ref(false);
-const loadImage = () => {
-    let input = document.createElement('input');
-}
-
-const showNav = () => {
-    document.getElementById('nav').style.transform = 'translateY(150%)';
-    setTimeout(() => {
-        showNavFile.value = !showNavFile.value; 
-    }, 300)
-}
+ 
+const closeNavFile = ref(false);
 const downloadImage = (id) => {
     document.getElementById(""+id).click();
 }
 const onFileChange = (event) => {
     const selectedFile = event.target.files[0];
     imageSrc.value = URL.createObjectURL(selectedFile);
-    document.getElementById('navFile').style.transform = 'translateY(150%)';
-    setTimeout(() => {
-        showNavFile.value = !showNavFile.value; 
-    }, 300)
+
+    closeNavFile.value = !closeNavFile.value;
+    
 
     // const img = new FormData();
     // img.append('photo', selectedFile);
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-if="!showNavFile" id="nav" class="transition-all duration-300 w-full h-[50px] animate-[showNav_0.3s_ease_1] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
+            <button class="bg-[url('/images/download.svg')]"  @click="showNav()"></button>
+            <button class="bg-[url('/images/help.svg')]"></button>
+            <button class="bg-[url('/images/top.svg')]"></button>
+            <button class="bg-[url('/images/profile.svg')]"></button>
+        </div>
+        <div v-if="showNavFile" id="navFile" class="w-full w-full transition-all duration-300 animate-[showNav_0.3s_ease_1] h-[50px] flex gap-4 justify-center items-center [&>button]:w-full [&>button]:h-full rounded-lg">
+            <button @click="downloadImage('camera')" class="bg-[#0A3D7F] rounded-lg text-white">Сделать фото</button>
+            <button @click="downloadImage('file') "  class="bg-[#0A3D7F] rounded-lg text-white">Выбрать из галереи</button>
+        </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  downloadImage: Function,
+  onFileChange: Function,
+  closeNavFile: Boolean
+})
+
+const showNavFile = ref(false);
+
+watch(() => props.closeNavFile, () => {
+    document.getElementById('navFile').style.transform = 'translateY(150%)';
+    setTimeout(() => {
+        showNavFile.value = !showNavFile.value; 
+    }, 300)
+})
+
+ 
+
+ const showNav = () => {
+    document.getElementById('nav').style.transform = 'translateY(150%)';
+    setTimeout(() => {
+        showNavFile.value = !showNavFile.value; 
+    }, 300)
+}
+</script>
