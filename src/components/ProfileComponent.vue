ef55230c5919952ad4fc4b3ace95f4745df6e311
coronelee <a.tirki90@gmail.com>
2024-06-13 09:29:06 +0300

    add

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -22,7 +22,7 @@ const telegram = new TelegramWebAppContainer();
 
 telegram.WebApp.ready();
 const first_name = telegram.WebApp.initDataUnsafe?.user?.first_name;
-const photo = telegram.WebApp.initDataUnsafe?.photo_url?.photo_url;
+const photo = telegram.WebAppUser?.initDataUnsafe?.user?.photo_url;
 
 
 </script>
