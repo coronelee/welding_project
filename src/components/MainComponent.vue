c80808e440cb9547173335bc78e5b68200903713
coronelee <a.tirki90@gmail.com>
2024-06-13 05:24:31 +0300

    ad

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,11 +1,38 @@
 <template>
+<input type="file" id="file" class="hidden" @change="onFileChange">
+<input type="file" id="camera" accept="image/*" capture="camera" class="hidden" @change="onFileChange">
      <div class="w-full h-full flex justify-center items-center flex-col p-2 gap-2 animate-[showMain_0.5s_ease_1]">
-        <div class="text-3xl font-mono w-full h-full bg-gray-300">img</div>
-        <div class="w-full h-[50px] flex gap-2 [&>button]:w-full [&>button]:h-full [&>button]:bg-gray-200">
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+        <div class="text-3xl font-mono w-full h-full bg-gray-300"><img :src="imageSrc" alt="" class="w-full h-full"></div>
+        <div class="w-full h-[50px] flex gap-2 [&>button]:w-full [&>button]:h-full   [&>button]:bg-contain [&>button]:bg-center [&>button]:bg-no-repeat rounded-lg">
+            <button class="bg-[url('/images/download.svg')]"  @click="downloadImage('file')"></button>
+            <button class="bg-[url('/images/help.svg')]"></button>
+            <button class="bg-[url('/images/top.svg')]"></button>
+            <button class="bg-[url('/images/profile.svg')]"></button>
         </div>
      </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const imageSrc = ref('/images/noImage.jpg');
+const loadImage = () => {
+    let input = document.createElement('input');
+}
+const downloadImage = (id) => {
+    document.getElementById(""+id).click();
+}
+const onFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    imageSrc.value = URL.createObjectURL(selectedFile);
+
+
+
+    // const img = new FormData();
+    // img.append('photo', selectedFile);
+    // axios.post(`http://localhost:8081/api/v1/photo/load`, 
+    // img,
+    // {
+    //     headers: { "Content-Type": "multipart/form-data"},
+    // },)
+  }
+</script>
