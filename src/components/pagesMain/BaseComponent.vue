405a4aa9d97f8bd8ac7c80e3810b7207d2025e72
coronelee <a.tirki90@gmail.com>
2024-06-15 00:18:41 +0300

    add

--- src/components/pagesMain/BaseComponent.vue
+++ src/components/pagesMain/BaseComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-3xl w-full h-full bg-white rounded-t-lg flex justify-between items-center  py-6 px-4  flex-col">
-        <div class="flex justify-center items-center flex-col gap-2  w-full">
+        <div class="flex justify-center items-center flex-col gap-2 w-full">
             <span class="font-Manrope_Bold text-[24px]">База знаний</span>
             <div
                 class="font-Manrope_Medium text-[16px] w-full border border-[#6BACE4] rounded-lg flex justify-center items-center flex-col">
@@ -38,20 +38,19 @@
                     </div>
                     <ChartComponent typechart="bar" />
                 </div>
-                <div
-                    class="font-Manrope_Medium text-[16px] w-full border border-[#6BACE4] rounded-lg flex justify-center items-center flex-col">
-                    <span class="w-full py-2 bg-[#6BACE4] text-white rounded-t-lg px-4 "
-                        @click="openLineChart('doughnut')">
-                        Число дефектов/период
-                    </span>
-                    <div id="doughnut" class="hidden ">
-                        <div class="flex flex-col px-4 font-Manrope_Medium text-[16px] text-[#5F5F5F]">
-                            <span class="">
-                                За сегодня: $имя дефекта - $число
-                            </span>
-                        </div>
-                        <ChartComponent typechart="doughnut" />
+            </div>
+            <div
+                class="font-Manrope_Medium text-[16px] w-full border border-[#6BACE4] rounded-lg flex justify-center items-center flex-col">
+                <span class="w-full py-2 bg-[#6BACE4] text-white rounded-t-lg px-4 " @click="openLineChart('doughnut')">
+                    Число дефектов/период
+                </span>
+                <div id="doughnut" class="hidden ">
+                    <div class="flex flex-col px-4 font-Manrope_Medium text-[16px] text-[#5F5F5F]">
+                        <span class="">
+                            За сегодня: $имя дефекта - $число
+                        </span>
                     </div>
+                    <ChartComponent typechart="doughnut" />
                 </div>
             </div>
         </div>
