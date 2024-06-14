ebb3ff9dc1e5f9adc9451bada5f1d908a6196434
coronelee <a.tirki90@gmail.com>
2024-06-15 00:45:43 +0300

    add

--- src/components/pagesMain/charts/ChartComponent.vue
+++ src/components/pagesMain/charts/ChartComponent.vue
@@ -3,21 +3,30 @@ import { Chart, registerables } from 'chart.js';
 import { computed } from 'vue';
 import { LineChart, BarChart, DoughnutChart } from 'vue-chart-3';
 let rand = [];
-const random = () => {
-    for (let i = 0; i < 40; i++) {
-        rand.push(Math.floor(Math.random() * 100))
-    }
-}
-
-random()
-Chart.register(...registerables);
-
+let label = [];
 const props = defineProps({
     typechart: String
 })
+const random = () => {
+
+    for (let i = 0; props.typechart == 'doughnut' ? i < 7 : i < 40; i++) {
+        rand.push(Math.floor(Math.random() * 100))
+    }
+}
+const labelCreate = () => {
+
+    for (let i = 0; props.typechart == 'doughnut' ? i < 7 : i < 30; i++) {
+        label.push(i)
+    }
+}
+labelCreate()
+random()
+Chart.register(...registerables);
+
+
 
 const chartData = computed(() => ({
-    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',],
+    labels: label,
     datasets: [
         {
             data: rand,
